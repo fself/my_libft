abba8f834853e2810e179ba1a625f9ecd9ba9a40
Francisco Self <fself@ox-n4.21-school.ru>
2019-09-20 22:43:45 +0300

    1

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -10,18 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strcmp(const char *str1, const char *str2)
+int	ft_strcmp(const char *s1, const char *s2)
 {
 	int				i;
-	unsigned char	s1;
-	unsigned char	s2;
+	unsigned char	str1;
+	unsigned char	str2;
 
 	i = 0;
-	while (str1[i] == str2[i] && str1[i] != '\0' && str2[i] != '\0')
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
 	{
 		i++;
 	}
-	s1 = (unsigned char)str1[i];
-	s2 = (unsigned char)str2[i];
+	str1 = (unsigned char)s1[i];
+	str2 = (unsigned char)s2[i];
 	return (s1 - s2);
 }
--- libft.h
+++ libft.h
@@ -52,7 +52,7 @@ char			*ft_strdup(const char *s);
 size_t			ft_strlcat(char *dst, const char *src, size_t size);
 char			*ft_strrchr(const char *s, int c);
 char			*ft_strstr(const char *haystack, const char *needle);
-int				ft_strcmp(const char *str1, const char *str2);
+int				ft_strcmp(const char *s1, const char *s2);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
 char			*ft_strnstr(const char *haystack, const char *needle, \
 		size_t len);
